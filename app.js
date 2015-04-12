a8d540087e11dc2f78536ce8803855cff89cf768
pgulley <nano3.14@gmail.com>
2015-04-12 14:11:32 -0400

    ugh

--- app.js
+++ app.js
@@ -17,4 +17,8 @@ server.listen(config.port, function () {
   	console.log('Sandbox running at %s:%s', host, port);
 });
 
+app.use(function(req, res, next) {
+  res.status(404).send('!');
+});
+
 module.exports = app;
