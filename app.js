1cde479894f3372583b983aed6dc8276fececdb4
pgulley <nano3.14@gmail.com>
2015-04-12 13:50:40 -0400

    this should fix heroku problems

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var app = express();
+var config = require("./config");
+
 
 app.get('/', function (req, res) {
   res.send('Hello World!, I work!');
 });
 
-var server = app.listen(3000, function () {
+var server = require('http').createServer(app)
+server.listen(config.port, function () {
 
   var host = server.address().address;
   var port = server.address().port;
---
+++ config.js
@@ -0,0 +1,19 @@
+var config;
+if(process.env.NODE_ENV === 'production') {
+    //Production environment variables
+    config = {
+        "request_timeout":2200,
+        "port":process.env.PORT,
+        "app_name":process.env.APP_NAME.toString(),
+        "URL":process.env.URL
+    }
+}
+else {
+    //Development environment variables
+    config = {
+        "port":3000,
+        "URL":"https://localhost:3000",
+
+    }
+};
+module.exports = config;
